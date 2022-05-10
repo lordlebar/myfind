d39c668dcc009adbe056810cf27e22886412f2bc
Corentin Lebarilier <corentin.lebarilier@epita.fr>
2022-05-10 21:36:22 +0200

    add

--- src/main.c
+++ src/main.c
@@ -5,16 +5,18 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     struct stat *hdr = (struct stat *)malloc(sizeof(struct stat));
     int status = 0;
     
     char *path = "";
-    if(argc >= 2){
+    if (argc >= 2){
         for (int i = 1; i < argc; i++)
         {
             status = stat(argv[i], hdr);
+            path = argv[i];
+
             if (status == -1)
             {
                 printf("myfind: ");
@@ -22,7 +24,6 @@ int main(int argc, char* argv[])
             }
             else if ((hdr->st_mode & S_IFMT) == S_IFDIR)
             {
-                path = argv[i];
                 myfind(path);
             }
             else if ((hdr->st_mode & S_IFMT) == S_IFREG)
@@ -31,10 +32,16 @@ int main(int argc, char* argv[])
                 printf("error: no directory or no regular file");
         }
     }
+    else if (argc == 1)
+    {
+        status = stat(".", hdr);
+        path = ".";
+
+        myfind(path);
+    }
     else
         printf("usage: ./myfind [path]");
 
     free(hdr);
-
     return 0;
 }
